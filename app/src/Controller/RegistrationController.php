e05d0a135e10af15eae9b79996d31b466c679ab7
etienne <etienne.mentrel@scafe.io>
2024-04-15 18:01:52 +0200

    Refactor registration controller to use request data for user creation

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -2,17 +2,24 @@
 // src/Controller/RegistrationController.php
 namespace App\Controller;
 
-// ...
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
-    public function index(UserPasswordHasherInterface $passwordHasher): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
-        // ... e.g. get the user data from a registration form
-        $user = new User(...);
-        $plaintextPassword = ...;
+        // get the user data from a registration form
+        $login = $request->request->get('login');
+        $email = $request->request->get('email');
+        $firstname = $request->request->get('firstname');
+        $lastname = $request->request->get('lastname');
+        $plaintextPassword = $request->request->get('password');
+
+        $user = new User($login, $plaintextPassword, $email, $firstname, $lastname);
 
         // hash the password (based on the security.yaml config for the $user class)
         $hashedPassword = $passwordHasher->hashPassword(
@@ -21,7 +28,9 @@ class RegistrationController extends AbstractController
         );
         $user->setPassword($hashedPassword);
 
-        // ...
+        // TODO: Save the user to the database and return a response
+
+        return new Response('User registered successfully.');
     }
 }
 ?>
