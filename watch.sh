cf5acd0d3625c69195cd1f60e064b4abdb88d7b0
etienne <etienne.mentrel@scafe.io>
2024-04-23 17:01:42 +0200

    Update watch.sh script to wait for user to finish writing before running doctrine:schema:update command

--- watch.sh
+++ watch.sh
@@ -8,16 +8,19 @@ CHECKSUM=$(find $WATCH_DIR -type f | sort | xargs md5sum)
 
 while true
 do
+    # Wait for a minute before checking again
+    sleep 30
+
     # Get the current checksum of the directory
     CURRENT_CHECKSUM=$(find $WATCH_DIR -type f | sort | xargs md5sum)
 
-    # If the checksum has changed, run the doctrine:schema:update command
+    # If the checksum has changed, wait for a few seconds to ensure the user has finished writing, then run the doctrine:schema:update command
     if [ "$CHECKSUM" != "$CURRENT_CHECKSUM" ]
     then
+        # Wait for a few seconds to ensure the user has finished writing
+        sleep 5
+
         cd /var/www/my_project && php bin/console doctrine:schema:update --force
         CHECKSUM=$CURRENT_CHECKSUM
     fi
-
-    # Wait for a minute before checking again
-    sleep 60
 done
