c00cf14da5fdb32ab03f7bee58f5ee8283638d7d
etienne <etienne.mentrel@scafe.io>
2024-04-25 11:37:40 +0200

    Add route and method for updating user information in UserController

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -9,10 +9,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use App\Entity\User;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class UserController extends AbstractController
 {
@@ -76,6 +77,53 @@ class UserController extends AbstractController
 
         // If no user is authenticated, throw an exception
         throw new AuthenticationException('No user is authenticated.');
+    }
+
+    #[Route('/api/user', name: 'update_user', methods: ['PUT'])]
+    public function update(Request $request, LoggerInterface $logger, JWTTokenManagerInterface $jwtManager, EntityManagerInterface $entityManager): Response
+    {
+        $logger->info('Entering the update method.');
+
+        // Get the JWT from the request
+        $jwt = $request->headers->get('Authorization');
+
+        // Define the decoding key and the allowed algorithms
+        $publicKey = file_get_contents(__DIR__ . '/../../config/jwt/public.pem');
+
+        // Decode the JWT
+        $decodedJwt = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+
+        // Get the current user
+        $user = $this->userRepository->findOneByEmail($decodedJwt->username);
+
+        // If the user does not exist, return a 404 response
+        if (!$user) {
+            return $this->json(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        // Get the request data
+        $data = json_decode($request->getContent(), true);
+
+        // Update the user fields
+        if (isset($data['email'])) {
+            $user->setEmail($data['email']);
+        }
+        if (isset($data['login'])) {
+            $user->setLogin($data['login']);
+        }
+        if (isset($data['firstname'])) {
+            $user->setFirstname($data['firstname']);
+        }
+        if (isset($data['lastname'])) {
+            $user->setLastname($data['lastname']);
+        }
+
+        // Save the updated user to the database
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Return a successful response
+        return $this->json(['message' => 'User updated successfully']);
     }
 }
 ?>
