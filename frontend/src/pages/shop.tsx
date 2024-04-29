92fc085fa1d8a70a614ee8593f546c0123b36486
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-04-29 21:35:51 +0200

    Fix login link in navbar

--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -1,39 +1,48 @@
 import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {Card, Image, CardFooter, CardBody} from "@nextui-org/react";
 
 export function Shop() {
+  const productPrice = "Product Price"; // Define your variable here
+
   return (
-    <Navbar>
-      <NavbarBrand>
-        <p className="font-bold text-inherit">ECLAT SHOP</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Catalog
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Contact
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            About us
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/login">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="/register" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+    <>
+      <div>
+        <Navbar>
+          <NavbarBrand>
+            <p className="font-bold text-inherit">ECLAT SHOP</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Catalog
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="#" aria-current="page">
+                Contact
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                About us
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem className="hidden lg:flex">
+              <Link href="/login">Logiqsdqsdn</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button as={Link} color="primary" href="/register" variant="flat">
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      </div>
+
+    </>
+
   );
 }
