6413d0d146f4012cb1ea3643394af3e093885bac
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-04-29 22:42:26 +0200

    Add new feature to the shop page

--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -6,43 +6,51 @@ export function Shop() {
   const list = [
     {
       title: "Orange",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg",
-    price: "$5.50",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg",
+      price: "$5.50",
+      description: "Test description"
     },
     {
       title: "Tangerine",
       img: "https://as1.ftcdn.net/v2/jpg/00/29/69/78/1000_F_29697857_Rh1QHZRduOv5u8KclCX0cGTPpkVPbtFt.jpg",
       price: "$3.00",
+      description: "Test description"
     },
     {
       title: "Raspberry",
       img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Raspberry_-_whole_%28Rubus_idaeus%29.jpg/1200px-Raspberry_-_whole_%28Rubus_idaeus%29.jpg",
       price: "$10.00",
+      description: "Test description"
     },
     {
       title: "Lemon",
       img: "https://media.istockphoto.com/id/1389128157/fr/photo/fruit-citronn%C3%A9-avec-feuille-isol%C3%A9e-citron-entier-et-demi-avec-des-feuilles-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=Ilhfen4bwWI5Xfp7A9EoxcEZ7jm_kjsTlI9eL-y-AzI=",
       price: "$5.30",
+      description: "Test description"
     },
     {
       title: "Avocado",
       img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/avocados-title-c32b587.jpg?quality=90&resize=556,505",
       price: "$15.70",
+      description: "Test description"
     },
     {
       title: "Lemon 2",
       img: "https://static.toiimg.com/thumb/msid-103846641,width-1280,height-720,resizemode-4/103846641.jpg",
       price: "$8.00",
+      description: "Test description"
     },
     {
       title: "Banana",
       img: "https://paniersduprimeur.fr/wp-content/uploads/2021/04/banane.png",
       price: "$7.50",
+      description: "Test description"
     },
     {
       title: "Watermelon",
       img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Watermelons.jpg/800px-Watermelons.jpg",
       price: "$12.20",
+      description: "Test description"
     },
   ];
 
@@ -96,10 +104,11 @@ export function Shop() {
                 className="w-full object-cover h-[140px]"
                 src={item.img}
               />
+              <p className="text-center">{item.description}</p>
             </CardBody>
             <CardFooter className="text-small justify-between">
               <b>{item.title}</b>
-              <p className="text-default-500">{item.price}</p>
+              <p className="text-default-500 text-lg">{item.price}</p>
             </CardFooter>
           </Card>
         ))}
