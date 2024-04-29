502274f471a6ae9f7c812de4867f7a10e2977938
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-04-29 15:14:25 +0200

    Update package.json and frontend pages with new dependencies and navbar component

--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 
-export const Shop = () => {
+export function Shop() {
   return (
-    <div>
-      <h1>Shop</h1>
-    </div>
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">ECLAT SHOP</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Catalog
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Contact
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About us
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   );
-};
-
-export default Shop;
+}
