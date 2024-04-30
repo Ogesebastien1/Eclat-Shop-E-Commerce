91f5ce6d7c0abbbafca7315b788d411e577aa0dc
etienne <etienne.mentrel@scafe.io>
2024-04-30 16:04:10 +0200

    Update composer.json, composer.lock, User.php, themeswitcher.tsx, package.json, UserController.php, and docker-compose.yaml with new dependencies and configurations

--- frontend/src/components/Checkout.tsx
+++ frontend/src/components/Checkout.tsx
@@ -1,13 +1,99 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
-import { Button } from "@nextui-org/react";
+import { useState, useRef, useEffect } from "react";
+import { useStripe } from "@stripe/react-stripe-js";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Divider,
+  Link,
+} from "@nextui-org/react";
+import Lottie from "lottie-react";
+import buy_animation from "../animations/buy-animation.json";
+import anime from "animejs";
 
-const CheckoutForm = () => {
+export default function CheckoutForm({
+  sessionId,
+  productResume,
+}: {
+  sessionId: any;
+  productResume: any;
+}) {
+  const stripe = useStripe();
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const buttonRef = useRef(null);
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    setButtonClicked(true);
+
+    if (!stripe) {
+      return;
+    }
+
+    const { error } = await stripe.redirectToCheckout({
+      sessionId,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+    }
+  };
+
+  useEffect(() => {
+    anime({
+      targets: buttonRef.current,
+      translateY: [-250, 0],
+      duration: 300, // Réduire la durée de l'animation
+    });
+  }, []);
+
   return (
-    <form>
-      <PaymentElement />
-      <Button color="primary">Pay</Button>
-    </form>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Link
+        href="/"
+        style={{ position: "absolute", top: "1rem", left: "1rem" }}
+      >
+        ← Back to Home
+      </Link>
+      <Card className="max-w-[400px]">
+        <CardHeader className="center flex-col">
+          <h1 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-pink-600">
+            CHECKOUT
+          </h1>
+          <Lottie animationData={buy_animation} loop={true} />
+        </CardHeader>
+        <Divider />
+        <CardBody>{productResume}</CardBody>
+        <Divider />
+        <CardFooter
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1rem",
+            marginBottom: "1rem",
+          }}
+        >
+          <Button
+            ref={buttonRef}
+            onClick={handleSubmit}
+            className={`bg-gradient-to-r from-blue-600 to-pink-600 w-full`}
+          >
+            Checkout
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
-};
-
-export default CheckoutForm;
+}
--- frontend/src/pages/payment.tsx
+++ frontend/src/pages/payment.tsx
@@ -1,26 +1,58 @@
+import { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 import CheckoutForm from "../components/Checkout";
-
+import { Spinner } from "@nextui-org/react";
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(
-  "pk_test_51H2nYdB8uQTceI1zCNSKq7fXG5JB6quW5TKClI00K2uxa6L2Wxe1sVHjy49BdSykYBLy3EwkfeEHYSeFSbwqjomM00Qm2uO5AS"
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || ""
 );
 
-export default function Payment() {
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-  };
+export default function Payment({
+  currency = "eur",
+  productName = "Test Product",
+  unitAmount = 1099,
+  quantity = 1,
+  productResume = "Test Product - 10.99€",
+}) {
+  const [sessionId, setSessionId] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  console.log(options);
+  useEffect(() => {
+    axios
+      .post("http://localhost:8000/api/payment/create-checkout-session", {
+        items: [
+          {
+            currency: currency,
+            productName: productName,
+            unitAmount: unitAmount,
+            quantity: quantity,
+          },
+        ],
+      })
+      .then((res) => {
+        setSessionId(res.data.id);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner color="default" />
+      </div>
+    );
+  }
 
   return (
-    <>
-      <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm />
-      </Elements>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      {sessionId && (
+        <Elements stripe={stripePromise}>
+          <CheckoutForm sessionId={sessionId} productResume={productResume} />
+        </Elements>
+      )}
+    </div>
   );
 }
