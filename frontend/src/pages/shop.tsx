c5b409be6868fc64fb7048cdeeb1f4164532662e
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-05-02 18:53:13 +0200

    Fetch card done

--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -4,8 +4,16 @@ import {Card, Image, CardFooter, CardBody} from "@nextui-org/react";
 import { Link as RouterLink } from 'react-router-dom';
 import MyNavbar from "../components/navbar";
 
+interface Item {
+  photo: string;
+  name: string;
+  description: string;
+  price: string;
+  id: string;
+}
+
 export function Shop() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState<Item[]>([]);
 
   useEffect(() => {
       fetch('http://localhost:8000/api/products', {
@@ -22,40 +30,39 @@ export function Shop() {
         .catch(error => console.error('Error:', error));
     }, []);
 
+    const items = list.map((item: Item) => (
+      <Card
+    isFooterBlurred
+    radius="lg"
+    className="border-none"
+  >
+    <Image
+      alt={item.name}
+      className="object-cover"
+      height={200}
+      src={item.photo}
+      width={200}
+    />
+    <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      <p className="text-tiny text-white/80">Price {item.price}</p>
 
-    
+      <RouterLink to={`/details/${item.id}`}>
+        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+          View Details
+        </Button>
+      </RouterLink>
+    </CardFooter>
+  </Card>
+));
+
   return (
     <>
     <MyNavbar />
-    {/* <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="gap-4 grid grid-cols-2 sm:grid-cols-4 mx-auto">
-      {list.map((item, index) => (
-          <Card
-            isFooterBlurred
-            radius="lg"
-            className="border-none"
-          >
-            <Image
-              alt={item.name}
-              className="object-cover"
-              height={200}
-              src={item.photo}
-              width={200}
-            />
-            <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-              <p className="text-tiny text-white/80">Price {item.price}</p>
-
-              <RouterLink to={`/details/${item.id}`}>
-                <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-                  Show more
-                </Button>
-              </RouterLink>
-            </CardFooter>
-          </Card>
-        ))}
+        {items}
       </div>
-    </div> */}
+    </div>
   </>
-
   );
 }
