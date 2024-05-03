71d0dc61f50ff8122ea5b7da764d59b990d1d32f
etienne <etienne.mentrel@scafe.io>
2024-05-03 13:04:34 +0200

    chore: Update success and cancel URLs in PaymentController and App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
@@ -7,6 +7,7 @@ import { Register } from "./pages/register";
 import { ResetPassword } from "./pages/reset-password";
 import { ModifiePassword } from "./pages/modifie-password";
 import { Shop } from "./pages/shop";
+import { Success } from "./pages/success";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { NextUIProvider } from "@nextui-org/react";
@@ -14,10 +15,13 @@ import ThemeSwitcher from "./components/themeswitcher";
 import { useTheme } from "./contexts/themeContext";
 import Payment from "./pages/payment";
 import DeliveryPage from "./pages/delivery";
+import Spline from "@splinetool/react-spline";
+import { useLocation } from "react-router-dom";
 
 function App() {
   const { theme } = useTheme();
   const themeClass = theme === "dark" ? "dark" : "light";
+  const location = useLocation();
 
   useEffect(() => {
     console.log("Theme changed to", theme);
@@ -26,8 +30,14 @@ function App() {
   return (
     <NextUIProvider>
       <ThemeSwitcher />
-      <main className={`${themeClass} text-foreground bg-background`}>
-        <div className="App">
+      <main className={`${themeClass} text-foreground bg-background relative`}>
+        {location.pathname !== "/" && (
+          <Spline
+            scene="https://prod.spline.design/Vr2Pm5sxZFITlfcj/scene.splinecode"
+            className="absolute inset-0 z-0"
+          />
+        )}
+        <div className="App relative z-10">
           <ToastContainer />
           <Routes>
             <Route path="/" element={<Home />} />
@@ -38,6 +48,7 @@ function App() {
             <Route path="/shop" element={<Shop />} />
             <Route path="/payment" element={<Payment />} />
             <Route path="/delivery" element={<DeliveryPage />} />
+            <Route path="/success" element={<Success />} />
           </Routes>
         </div>
       </main>
