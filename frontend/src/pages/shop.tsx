4d0843a5e062deca3c50ca1c34704affe26c5c68
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-05-06 14:25:58 +0200

    chore: Update navbar component with useNavigate and hover functionality

--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -44,38 +44,6 @@ export function Shop() {
       })
       .catch(error => console.error('Error:', error));
   }, []);
-
-  //  const items = list.map((item: Item, index: number) => (
-  //   <Card
-  //     isFooterBlurred
-  //     radius="lg"
-  //     className="border-none"
-  //   >
-  //     <Image
-  //       alt={item.name}
-  //       className="object-cover"
-  //       height={200}
-  //       src={item.photo}
-  //       width={200}
-  //     />
-  //     <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-  //       <p className="text-tiny text-white/80">{item.price} $</p>
-  //       <Button 
-  //         className="text-tiny text-white bg-black/20" 
-  //         variant="flat" 
-  //         color="default" 
-  //         radius="lg" 
-  //         size="sm"
-  //         onClick={() => navigate(`/details/${item.id}`, { state: { item } })}
-  //         onMouseEnter={() => handleMouseEnter(index)}
-  //         onMouseLeave={() => handleMouseLeave(index)}
-  //         style={hoverStates[index] ? { backgroundColor: '#0067dd' } : {}}
-  //       >
-  //         Go to details
-  //       </Button>
-  //     </CardFooter>
-  //   </Card>
-  // ));
   
   return (
     <>
