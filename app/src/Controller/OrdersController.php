b25f23bfd686db7103af04957dad766f05e301a9
etienne <etienne.mentrel@scafe.io>
2024-05-07 15:38:51 +0200

    feat: Add JWT authentication for user orders endpoint

--- app/src/Controller/OrdersController.php
+++ app/src/Controller/OrdersController.php
@@ -8,7 +8,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\OrdersRepository;
 use Psr\Log\LoggerInterface;
-
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 class OrdersController extends AbstractController
 {
     #[Route('/api/orders/{uuid}', name: 'order_show', methods: ['GET'])]
@@ -45,6 +48,61 @@ class OrdersController extends AbstractController
                 ];
             }) -> toArray()
         ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    #[Route('/api/orders/', name: 'user_orders', methods: ['GET'])]
+    public function showUserOrders(UserRepository $userRepository, OrdersRepository $ordersRepository, LoggerInterface $logger, Request $request): JsonResponse
+    {
+        // Get the JWT from the request
+        $jwt = $request->headers->get('Authorization');
+
+        // Define the decoding key and the allowed algorithms
+        $publicKey = file_get_contents(__DIR__ . '/../../config/jwt/public.pem');
+
+        // Decode the JWT
+        $decodedJwt = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+
+        // Get the current user
+        $user = $userRepository->findOneByEmail($decodedJwt->username);
+
+        if (!$user) {
+            $logger->info("No user found with email {$decodedJwt->username}.");
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $orders = $ordersRepository->findByUser($user->getId());
+
+        if (!$orders) {
+            $logger->info("No orders found for user with ID {$user->getId()}.");
+            return new JsonResponse(['message' => 'No orders found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = array_map(function($order) {
+            $user = $order->getUser();
+            return [
+                'id' => $order->getId(),
+                'totalPrice' => $order->getTotalPrice(),
+                'creationDate' => $order->getCreationDate()->format('Y-m-d H:i'),
+                'uuid' => $order->getUuid(),
+                'user' => [
+                    'id' => $user->getId(),
+                    'username' => $user->getUsername(),
+                    'email' => $user->getEmail(),
+                ],
+                "item" => $order -> getItems() -> map(function($item){
+                    return [
+                        'id' => $item -> getId(),
+                        'product' => $item -> getProduct() -> getName(),
+                        'quantity' => $item -> getQuantity(),
+                        'description' => $item -> getProduct() -> getDescription(),
+                        'photo' => $item -> getProduct() -> getPhoto(),
+                        'price' => $item -> getProduct() -> getPrice()
+                    ];
+                }) -> toArray()
+            ];
+        }, $orders);
 
         return new JsonResponse($data, Response::HTTP_OK);
     }
