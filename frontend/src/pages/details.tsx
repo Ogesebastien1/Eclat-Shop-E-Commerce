793acba9ae6e0926d7dd1142f5664fdfe58bb23a
etienne <etienne.mentrel@scafe.io>
2024-05-11 02:44:28 +0200

    chore: Adjust image container size and improve layout in Details page

--- frontend/src/pages/details.tsx
+++ frontend/src/pages/details.tsx
@@ -36,7 +36,6 @@ export default function Details() {
       if (parentElement) {
         parentElement.style.width = "100%";
         parentElement.style.height = "100%";
-        parentElement.style.maxWidth = "none";
       }
     }
   }, []);
@@ -61,82 +60,94 @@ export default function Details() {
   return (
     <>
       <MyNavbar />
-      <Button className="m-4" onClick={() => window.history.back()}>
-        Back to Shop
-      </Button>
+      <Button onClick={() => window.history.back()}>Back to Shop</Button>
       <div
         style={{
           display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: "100px",
-          marginLeft: "250px",
-          marginRight: "20px",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90vh",
         }}
       >
-        <div style={{ flex: 1 }}>
-          <Accordion variant="splitted">
-            <AccordionItem
-              key="1"
-              aria-label="Product Details"
-              title="Product Details"
-            >
-              {item.description}
-            </AccordionItem>
-            <AccordionItem
-              key="2"
-              aria-label="Shipping Information"
-              title="Shipping Information"
-            >
-              "We offer free shipping worldwide. The product will be delivered
-              within 2-3 weeks."
-            </AccordionItem>
-            <AccordionItem
-              key="3"
-              aria-label="Return Policy"
-              title="Return Policy"
-            >
-              "We accept returns within 30 days of the purchase date."
-            </AccordionItem>
-          </Accordion>
-        </div>
-        <div style={{ flex: 1, width: "100%" }} className="w-full">
-          <Card
-            isFooterBlurred
-            radius="lg"
-            className="border-none"
-            style={{ width: "90%", height: "300px", marginBottom: "50px" }}
-          >
-            <Image
-              ref={imageRef}
-              alt={item.name}
-              className="object-cover w-full h-full imgpersonalized"
-              src={item.photo}
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
-            <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-              <p
-                className="text-tiny text-white/90"
-                style={{ marginLeft: "90px", fontSize: "20px", color: "white" }}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: "2rem",
+          }}
+        >
+          <div style={{ flex: 1, width: "100%" }}>
+            <Accordion variant="splitted" className="w-full pt-5">
+              <AccordionItem
+                key="1"
+                aria-label="Product Details"
+                title="Product Details"
+                className="break-words"
               >
-                {item.price} €
-              </p>
-              <Button
-                className="text-tiny text-white bg-white/20"
-                variant="flat"
-                color="default"
-                radius="lg"
-                size="sm"
-                onClick={(event) =>
-                  addToCart(Number(item.id), item.name, event)
-                }
+                {item.description}
+              </AccordionItem>
+              <AccordionItem
+                key="2"
+                aria-label="Shipping Information"
+                title="Shipping Information"
+                className="break-words"
               >
-                Add to Cart
-              </Button>
-            </CardFooter>
-          </Card>
+                "We offer free shipping worldwide. The product will be delivered
+                within 2-3 weeks."
+              </AccordionItem>
+              <AccordionItem
+                key="3"
+                aria-label="Return Policy"
+                title="Return Policy"
+                className="break-words"
+              >
+                "We accept returns within 30 days of the purchase date."
+              </AccordionItem>
+            </Accordion>
+          </div>
+          <div style={{ flex: 1 }} className="w-full pr-5">
+            <Card
+              isFooterBlurred
+              radius="lg"
+              className="border-none m-4"
+              style={{ width: "100%", minHeight: "100px", height: "auto" }}
+            >
+              <Image
+                ref={imageRef}
+                alt={item.name}
+                className="object-cover w-full h-full imgpersonalized"
+                src={item.photo}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <p
+                  className="text-tiny text-white/90"
+                  style={{
+                    marginLeft: "90px",
+                    fontSize: "20px",
+                    color: "white",
+                  }}
+                >
+                  {item.price} €
+                </p>
+                <Button
+                  className="text-tiny text-white bg-white/20"
+                  variant="flat"
+                  color="default"
+                  radius="lg"
+                  size="sm"
+                  onClick={(event) =>
+                    addToCart(Number(item.id), item.name, event)
+                  }
+                >
+                  Add to Cart
+                </Button>
+              </CardFooter>
+            </Card>
+            <Cart />
+          </div>
         </div>
-        <Cart />
       </div>
     </>
   );
